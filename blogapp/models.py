fa32261d67c6b258534b33362f01c716ef2122e5
terfo1 <alisher.temirhan@gmail.com>
2024-06-27 22:07:18 +0500

    imagebyvers

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django_ckeditor_5.fields import CKEditor5Field
-
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 def validate_word_count(value):
     words = value.split()
     if len(words) > 20:
@@ -20,5 +21,11 @@ class BlogPost(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    imageby=models.CharField(max_length=100)
+    author = models.CharField(max_length=100,default='admin')
     def __str__(self):
         return self.title
+@receiver(pre_save, sender=BlogPost)
+def update_author(sender, instance, **kwargs):
+    if not instance.author:
+        instance.author = 'admin'
